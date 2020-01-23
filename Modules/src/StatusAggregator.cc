98eafc92d8d7c8a8451a79348e7710962e549169
Christoph Kampmeyer <christoph.kampmeyer@desy.de>
2020-01-23 17:13:13 +0100

    Cleaned up, comments

--- Modules/src/StatusAggregator.cc
+++ Modules/src/StatusAggregator.cc
@@ -10,11 +10,17 @@ namespace ChimeraTK{
     // This does not work, we need to operate on the virtual hierarchy
     //std::list<Module*> subModuleList{getOwner()->getSubmoduleList()};
 
-    //auto subModuleList = getOwner()->findTag(".*").getSubmoduleList();
-    //      auto accList{module->getAccessorList()};
+    // This approach crashes
+//    auto subModuleList = getOwner()->findTag(".*").getSubmoduleList();
+
+//    for(auto module : subModuleList){
+
+//      std::cout << "Testing Module " << module->getName() << std::endl;
+//      auto accList{module->getAccessorList()};
+//    }
 
 
-
+    // Works, as long as we do not use HIerarchyModifiers other than "none" in the test app
     std::cout << "Populating aggregator " << getName() << std::endl;
 
     auto allAccessors{getOwner()->findTag(".*").getAccessorListRecursive()};
@@ -31,11 +37,7 @@ namespace ChimeraTK{
 
     }
 
-//    for(auto module : subModuleList){
 
-//      std::cout << "Testing Module " << module->getName() << std::endl;
-
-//      auto accList{module->getAccessorList()};
 
 //      if(dynamic_cast<StatusMonitor*>(module)){
 //        std::cout << "  Found Monitor " << module->getName() << std::endl;
